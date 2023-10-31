afb0a181d4666e3b869d58f5a16bceb2b5de2873
Denis Korotaevsky <huligan8771@gmail.com>
2023-10-31 20:11:43 +0300

    main

---
+++ script.js
@@ -0,0 +1,11 @@
+let swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    mousewheel: true,
+    keyboard: true,
+    loop: true,
+    slidesPerView: 3
+});
